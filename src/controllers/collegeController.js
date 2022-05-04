b01e8903ae668f8c949118550d50d03af5eca79b
JitendraSaktiman <jitendraop36@gmail.com>
2022-05-04 17:18:16 +0530

    ok

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -2,15 +2,4 @@
 const CollegeModel = require('../models/collegeModel');
 const InternModel = require('../models/internModels');
 
-
-// const createCollege = async function(req, res){
-
-//     try{
-//     let data = req.body;
-
-//     let savedData = await CollegeModel.create(data)
-//     }
-//     catch(err){
-//             res.status(500).send({status:false,msg:err.message})
-//     }
-// }
+ 
